cb05956572cbe064e69842e9dc1f8004809616a6
Sikstep <Sikstep1988@gmail.com>
2023-03-21 19:13:34 +0300

    Add ChangeTodolistTitle function to todolistReducer

--- src/Reducers/Todolists-reducer.ts
+++ src/Reducers/Todolists-reducer.ts
@@ -1,28 +1,39 @@
 import {TodoListType} from '../App';
 import {v1} from 'uuid';
 
+export type ActionTypes = RemoveTodolistAT | AddTodolistAT | ChangeTodolistTitleAT
+
 export type RemoveTodolistAT = {
-    type: "REMOVE-TODOLIST"
+    type: 'REMOVE-TODOLIST'
     id: string
 }
 
 export type AddTodolistAT = {
-    type: "ADD-TODOLIST"
+    type: 'ADD-TODOLIST'
     title: string
 }
 
-export const todolistsReducer = (todolists: TodoListType[], action: RemoveTodolistAT | AddTodolistAT): TodoListType[] => {
+export type ChangeTodolistTitleAT = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    todolistID: string
+    newTitle: string
+}
+
+export const todolistsReducer = (todolists: TodoListType[], action: ActionTypes): TodoListType[] => {
     switch (action.type) {
-        case "REMOVE-TODOLIST":
+        case 'REMOVE-TODOLIST':
             return todolists.filter(tl => tl.id !== action.id)
-        case "ADD-TODOLIST":
+        case 'ADD-TODOLIST':
             const newTodoListId = v1()
             const newTodoList: TodoListType = {
                 id: newTodoListId,
                 title: action.title,
-                filter: "all"
+                filter: 'all'
             }
             return [...todolists, newTodoList]
+        case 'CHANGE-TODOLIST-TITLE':
+            return todolists.map(tl => tl.id === action.todolistID ? {...tl, title: action.newTitle} : tl)
+
         default:
             return todolists
 
