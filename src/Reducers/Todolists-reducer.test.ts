ba4f9875ac7646af9b3aae903c70174e539048c0
Sikstep <Sikstep1988@gmail.com>
2023-03-21 18:51:05 +0300

    MaterialUI with reducer started. Create Todolists-reducer. Create UnitTest for Todolists-reducer

--- src/Reducers/Todolists-reducer.test.ts
+++ src/Reducers/Todolists-reducer.test.ts
@@ -1,2 +1,19 @@
+import {v1} from 'uuid';
+import { TodoListType } from '../App';
+import {todolistsReducer} from './Todolists-reducer';
 
+test('correct todolist should be removed', () => {
+    //
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+    const startState: TodoListType[] = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+    //
 
+    const endState = todolistsReducer(startState, {type:"REMOVE-TODOLIST", id: todolistId1} )
+    //
+    expect(endState.length).toBe(1);
+    expect(endState[0].id).toBe(todolistId2);
+});
--- src/Reducers/Todolists-reducer.ts
+++ src/Reducers/Todolists-reducer.ts
@@ -1,8 +1,15 @@
 import {TodoListType} from '../App';
 
+export type RemoveTodolistAC = {
+    type: "REMOVE-TODOLIST"
+    id: string
+}
 
-export const todolistsReducer = (todolists: TodoListType[]): TodoListType[] => {
-    switch () {
+export const todolistsReducer = (todolists: TodoListType[], action: any): TodoListType[] => {
+    switch (action.type) {
+        case "REMOVE-TODOLIST":
+            return todolists.filter(tl => tl.id !== action.id)
+
         default:
             return todolists
 
